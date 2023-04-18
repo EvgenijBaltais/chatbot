d82fdb8b8fb215021cabe2a734965dfaaaf394f8
EvgenijBaltais <evgenij.baltais@gmail.com>
2023-04-18 21:08:06 +0300

    first commit

--- dialog.js
+++ dialog.js
@@ -1,32 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const chat = {
+        
+        seconds: 0,
+
+        chatHtml: function () {
+            return `
+                <div class="chat">
+                    <h2 class = "chat-h2">Робот-помощник</h2>
+                    <div class="chat-ins"></div>
+                </div>`
+        },
 
 
+        makeStep: (text, buttons = []) => {
 
-    let phrases = {
-        hello: 'Добрый день! Буду рада помочь Вам оформить заказ.',
-        recall: 'Есть вопросы? Хотите перезвоним Вам и расскажем подробнее?',
-        choose: 'Вы выбрали набор ',
-        notFirstTime: 'Вы уже заказывали у нас?',
-        thankyou: 'Спасибо за Ваш заказ! Мы свяжемся с Вами в ближайшее время для подтверждения!'
+
+            let makeButton = text => `<button class = "chat-button">${text}</button>`
+
+            return `
+                <div class = "chat-step">
+                    <div class = "manager-foto" style = "background-image: url('/img/manager.jpg');"></div>
+                    <div class = "manager-text">${text}${buttons.length ? `<div>${makeButton(buttons[0])}${makeButton(buttons[1])}</div>` : ''}</div>
+                </div>`
+        },
+
+       phrases: {
+            hello: 'Добрый день! Буду рада помочь Вам оформить заказ.',
+            recall: 'Есть вопросы? Хотите перезвоним Вам и расскажем подробнее?',
+            howmany: 'Сколько зонтов вы хотите заказать?',
+            choose: 'Вы выбрали набор',
+            notFirstTime: 'Вы уже заказывали у нас?',
+            thankyou: 'Спасибо за Ваш заказ! Мы свяжемся с Вами в ближайшее время для подтверждения!'
+        },
+
+        btns: {
+            yes: `Да`,
+            no: `Нет`,
+            order: `Заказать`
+        },
+
+        init: function () {
+            document.body.insertAdjacentHTML('beforeend', this.chatHtml())
+            document.body.querySelector('.chat-ins').insertAdjacentHTML('beforeend', this.makeStep(this.phrases.hello))
+            document.body.querySelector('.chat-ins').insertAdjacentHTML('beforeend', this.makeStep(this.phrases.howmany, [this.btns.yes, this.btns.no]))
+
+
+        }
+
     }
 
 
-    let chatHtml = `
-        <div class="chat">
-            <h2 class = "chat-h2">Робот-помощник</h2>
-            <div class="chat-ins"></div>
-        </div>
-    `
-
-    let chatStep = `
-        <div class = "chat-step">
-            <div class = "manager-foto" style = "background-image: url('/img/manager.jpg');"></div>
-            <div class = "manager-text">lorm g e eeeg g wegwe gweg weg weg weg </div>
-        </div>
-    `
-
-    document.body.insertAdjacentHTML('beforeend', chatHtml)
-    document.body.querySelector('.chat-ins').insertAdjacentHTML('beforeend', chatStep)
-
+    chat.init()
 })
