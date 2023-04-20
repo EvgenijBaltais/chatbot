5a41881eb4a5fd7bf15eb6bb5de1b3a4a6a11936
EvgenijBaltais <evgenij.baltais@gmail.com>
2023-04-20 21:14:29 +0300

    1

--- index.php
+++ index.php
@@ -8,6 +8,32 @@
 <body>
 
 	<div class = "wrapper">
+		<nav class = "nav">
+			<div class="main-logo-w">
+				<img src="img/logo.png" class="main-logo" alt="">
+				<p class="main-logo-text">Umbrella Inc.</p>
+			</div>
+			<ul class="nav-ul">
+				<li class="nav-li">
+					<a href="" class="nav-li-a">Преимущества</a>
+				</li>
+				<li class="nav-li">
+					<a href="" class="nav-li-a">Подробнее</a>
+				</li>
+				<li class="nav-li">
+					<a href="" class="nav-li-a">Доставка</a>
+				</li>
+				<li class="nav-li">
+					<a href="" class="nav-li-a">FAQ</a>
+				</li>
+				<li class="nav-li">
+					<a href="" class="nav-li-a">Дополнительно</a>
+				</li>
+				<li class="nav-li">
+					<a href="" class="nav-li-a">Контакты</a>
+				</li>
+			</ul>
+		</nav>
 
 		<div class = "first-block">
 			<div class="content">
