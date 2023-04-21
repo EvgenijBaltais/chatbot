6d32cbc204b8ebcfbebdf0c797040a88db9f2405
EvgenijBaltais <evgenij.baltais@gmail.com>
2023-04-21 20:52:29 +0300

    1

--- index.php
+++ index.php
@@ -36,8 +36,35 @@
 		</nav>
 
 		<div class = "first-block">
-			<div class="content">
-				
+			<div class="content content-top">
+				<div class = "top-info">
+					<h1 class = "h1-title">Купите надежный недорогой зонт</h1>
+					<h3 class = "h1-subtitle">Купите мужской или женский зонт</h3>
+				</div>
+				<div class = "top-slider">
+					<div class="slider-w">
+
+						<div class="glide">
+							<div class="glide__track" data-glide-el="track">
+								<ul class="glide__slides">
+									<li class="glide__slide">0</li>
+									<li class="glide__slide">1</li>
+									<li class="glide__slide">2</li>
+								</ul>
+							</div>
+							<div data-glide-el="controls">
+							  <button data-glide-dir="<<">Start</button>
+							  <button data-glide-dir=">>">End</button>
+							</div>
+							<div class="glide__bullets" data-glide-el="controls[nav]">
+								<button class="glide__bullet" data-glide-dir="=0"></button>
+								<button class="glide__bullet" data-glide-dir="=1"></button>
+								<button class="glide__bullet" data-glide-dir="=2"></button>
+							</div>
+						</div>
+
+					</div>
+				</div>
 			</div>
 		</div>
 
@@ -45,7 +72,12 @@
 			
 		</div>
 	</div>
+		<script src = "/node_modules/@glidejs/glide/dist/glide.min.js"></script>
+		<script>
+			import Glide from '/node_modules/@glidejs/glide/dist/glide.modular.esm.js'
 
+			new Glide('.glide').mount()
+		</script>
 	<!--<script src = "dialog.js"></script>-->
 </body>
 </html>
